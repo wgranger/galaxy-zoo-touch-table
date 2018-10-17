fd997a6628bf827689c0ad223caf66156b3f50bd
Will Granger <twgranger@gmail.com>
2018-10-17 14:20:16 -0500

    Solar System Example

--- GalaxyZooTouchTable/GalaxyZooTouchTable/Views/SpaceView.xaml.cs
+++ GalaxyZooTouchTable/GalaxyZooTouchTable/Views/SpaceView.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Media3D;
 
 namespace GalaxyZooTouchTable.Views
 {
@@ -7,9 +9,25 @@ namespace GalaxyZooTouchTable.Views
     /// </summary>
     public partial class SpaceView : UserControl
     {
+        SolarSystem3D SolarSystem;
+
         public SpaceView()
         {
             InitializeComponent();
+
+            view1.Camera.Position = new Point3D(0, 400, 500);
+            view1.Camera.LookDirection = new Vector3D(0, -400, -500);
+            SolarSystem = view1.Children[2] as SolarSystem3D;
+            DataContext = SolarSystem;
+
+            Loaded += new RoutedEventHandler(Window1_Loaded);
         }
+
+        void Window1_Loaded(object sender, RoutedEventArgs e)
+        {
+            SolarSystem.InitModel();
+            SolarSystem.UpdateModel();
+        }
+
     }
 }
