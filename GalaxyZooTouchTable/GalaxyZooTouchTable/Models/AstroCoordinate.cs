ef7eacbd465dad9c9e4fb6e251837e6edbf823b6
Will Granger <twgranger@gmail.com>
2018-10-26 14:29:06 -0500

    Add Astrometry

---
+++ GalaxyZooTouchTable/GalaxyZooTouchTable/Models/AstroCoordinate.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace GalaxyZooTouchTable.Models
+{
+    public class AstroCoordinate
+    {
+        public double RA { get; set; }
+        public double DEC { get; set; }
+        public double Redshift { get; set; }
+        public Declination Declination { get; set; }
+
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+
+        public AstroCoordinate(double ra, double dec, double redshift)
+        {
+            //RA = ra;
+            //DEC = dec;
+            RA = 26.017046;
+            DEC = -15.937469;
+            Redshift = 11.9;
+
+            ConvertDeclination(dec);
+            FindCartesianCoordinates();
+        }
+
+        public void ConvertDeclination(double dec)
+        {
+            bool IsNegative = dec < 0;
+            dec = Math.Abs(dec);
+
+            var degree = (int)Math.Round(dec);
+            var decM = (int)Math.Abs(Math.Round((dec - degree) * 60));
+            var decS = (int)Math.Round((Math.Abs((dec - degree) * 60) - decM) * 60);
+
+            var test = (Math.Abs(degree) + (decM / 60) + (decS / 3600));
+            Declination = new Declination(IsNegative, degree, decM, decS);
+        }
+
+        public double ToDegrees(double angle)
+        {
+            return angle * (Math.PI / 180.0);
+        }
+
+        public void FindCartesianCoordinates()
+        {
+            X = Redshift * Math.Cos(ToDegrees(DEC)) * Math.Cos(ToDegrees(RA));
+            Y = Redshift * Math.Cos(ToDegrees(DEC)) * Math.Sin(ToDegrees(RA));
+            Z = Redshift * Math.Sin(ToDegrees(DEC));
+        }
+    }
+
+    public class Declination
+    {
+        public int Degrees { get; set; }
+        public int ArcMin { get; set; }
+        public int ArcSec { get; set; }
+
+        public Declination(bool IsNeg, int degrees, int arcmin, int arcsec)
+        {
+            Degrees = IsNeg ? degrees * -1 : degrees;
+            ArcMin = arcmin;
+            ArcSec = arcsec;
+        }
+    }
+}
--- GalaxyZooTouchTable/GalaxyZooTouchTable/Views/Centerpiece.xaml.cs
+++ GalaxyZooTouchTable/GalaxyZooTouchTable/Views/Centerpiece.xaml.cs
@@ -1,4 +1,5 @@
-﻿using HelixToolkit.Wpf;
+﻿using GalaxyZooTouchTable.Models;
+using HelixToolkit.Wpf;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Media.Media3D;
@@ -17,16 +18,19 @@ namespace GalaxyZooTouchTable
             //view1.Camera.LookDirection = new Vector3D(0, 0, 0);
 
             viewport.DefaultCamera = new PerspectiveCamera();
-            viewport.DefaultCamera.Position = new Point3D(0.5, 0.5, 0.5);
-            viewport.DefaultCamera.LookDirection = new Vector3D(-0.5, -0.5, -0.5);
-            viewport.DefaultCamera.UpDirection = new Vector3D(0, 0, 1);
+            viewport.DefaultCamera.Position = new Point3D(0, 0, 0);
+            viewport.CameraMode = CameraMode.FixedPosition;
+            //viewport.DefaultCamera.LookDirection = new Vector3D(-0.5, -0.5, -0.5);
+            //viewport.DefaultCamera.UpDirection = new Vector3D(0, 0, 1);
 
             viewport.Children.Add(new GridLinesVisual3D());
 
+            var test = new AstroCoordinate(200.00726, 3.084285, 0.1469969);
+
             SphereVisual3D sphere = new SphereVisual3D();
-            sphere.Radius = 2;
-            sphere.Center = new Point3D(2, 0, 2);
-            sphere.Fill = Brushes.Red;
+            sphere.Radius = 0.25;
+            sphere.Center = new Point3D(test.X, test.Y, test.Z);
+            sphere.Fill = Brushes.Gray;
             viewport.Children.Add(sphere);
         }
     }
